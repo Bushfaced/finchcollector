241c7864d72e994a004094b81b3bf29df4718507
richardguzman <thisisrichardguzman@gmail.com>
2022-05-10 14:04:04 -0700

    lab part 3

--- main_app/models.py
+++ main_app/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 # Create your models here.
 class Finch(models.Model):
-  # First define the attributes/fields
   name = models.CharField(max_length=100)
   breed = models.CharField(max_length=100)
-  # Django will create inputs for a form
-  # TextField will create a <textarea>
   description = models.TextField(max_length=250)
   age = models.IntegerField()
 
@@ -16,3 +19,18 @@ class Finch(models.Model):
 
   def get_absolute_url(self):
     return reverse('detail', kwargs={'finch_id': self.id})
+
+class Feeding(models.Model):
+  date = models.DateField('feeding date')
+  meal = models.CharField(
+    max_length=1,
+	 choices=MEALS,
+	 default=MEALS[0][0]
+  )
+  Finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+
+  class Meta:
+    ordering = ['-date']
