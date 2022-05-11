ebd03964233cd90eddaf729533b4f27d48a8280b
richardguzman <thisisrichardguzman@gmail.com>
2022-05-11 11:15:18 -0700

    Lab part 5

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 MEALS = (
     ('B', 'Breakfast'),
@@ -7,18 +8,33 @@ MEALS = (
     ('D', 'Dinner')
 )
 
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
 # Create your models here.
 class Finch(models.Model):
   name = models.CharField(max_length=100)
   breed = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   age = models.IntegerField()
+  toys = models.ManyToManyField(Toy)
+
 
   def __str__(self):
     return f'{self.name} ({self.id})'
 
   def get_absolute_url(self):
     return reverse('detail', kwargs={'finch_id': self.id})
+
+  def fed_for_today(self):
+    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS) 
 
 class Feeding(models.Model):
   date = models.DateField('feeding date')
@@ -29,8 +45,8 @@ class Feeding(models.Model):
   )
   finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
 
-  def __str__(self):
-    return f"{self.get_meal_display()} on {self.date}"
-
   class Meta:
     ordering = ['-date']
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
